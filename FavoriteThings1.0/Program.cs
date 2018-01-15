9da32d6046c0622ecf732d075630010a0832abb5
Jess <jessibcoden@gmail.com>
2018-01-15 16:54:55 -0600

    added favorite classes and methods on muppets

--- FavoriteThings1.0/Program.cs
+++ FavoriteThings1.0/Program.cs
@@ -12,4 +12,110 @@ namespace FavoriteThings1._0
         {
         }
     }
+
+    class FavMuppet
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        private string _favTrait;
+        public string FavTrait
+        {
+            get
+            {
+                return _favTrait;
+            }
+            set
+            {
+                _favTrait = value;
+            }
+        }
+
+        //CONSTRUCTOR METHOD:
+        public FavMuppet(string muppetName)
+        {
+            _firstName = muppetName;
+        }
+
+        //METHOD:
+        public bool PostFavMuppet(string muppetName)
+        {
+            _firstName = muppetName;
+            return true;
+        }
+    }
+
+
+
+    class FavChild
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        private int _childAge;
+        public int ChildAge
+        {
+            get
+            {
+                return _childAge;
+            }
+            set
+            {
+                _childAge = value;
+            }
+        }
+    }
+
+    class FavClassmate
+    {
+
+        private string _classmateName = Console.ReadLine();
+        public string ClassmateName
+        {
+            get
+            {
+                return _classmateName;
+            }
+            set
+            {
+                _classmateName = value;
+            }
+        }
+
+        private int _classmateMaturityAge;
+        public int ClassmateMaturityAge
+        {
+            get
+            {
+                return _classmateMaturityAge;
+            }
+            set
+            {
+                _classmateMaturityAge = value;
+            }
+        }
+    }
 }
+
+
+
